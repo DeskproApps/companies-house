0e46f8d51c28659fcbba8be582b3ff2315e20eb0
Tweeno <itweeno@gmail.com>
2023-03-15 11:35:23 +0000

    improving ui

--- src/components/Property/Property.tsx
+++ src/components/Property/Property.tsx
@@ -1,19 +1,33 @@
 import React, { FC, ReactNode } from "react";
-import { useDeskproAppTheme, Stack, H2 } from "@deskpro/app-sdk";
+import { useDeskproAppTheme, Stack } from "@deskpro/app-sdk";
 
 export interface PropertyProps {
-    children: JSX.Element | ReactNode;
-    title?: string;
-    width?: string;
+  children: JSX.Element | ReactNode;
+  title?: string;
+  width?: string;
 }
 
-export const Property: FC<PropertyProps> = ({ title, children, width }: PropertyProps) => {
-    const { theme } = useDeskproAppTheme();
+export const Property: FC<PropertyProps> = ({
+  title,
+  children,
+  width,
+}: PropertyProps) => {
+  const { theme } = useDeskproAppTheme();
 
-    return (
-        <Stack vertical style={{ width: width ?? "auto" }}>
-            {title && <div style={{ color: theme.colors.grey80, fontSize: ".8rem", marginBottom: "2px" }}>{title}</div>}
-            <div style={{ fontSize: "12px" }}>{children}</div>
-        </Stack>
-    );
+  return (
+    <Stack vertical style={{ width: width ?? "auto" }}>
+      {title && (
+        <div
+          style={{
+            color: theme.colors.grey80,
+            fontSize: ".8rem",
+            marginBottom: "2px",
+          }}
+        >
+          {title}
+        </div>
+      )}
+      <div style={{ fontSize: "12px" }}>{children}</div>
+    </Stack>
+  );
 };
