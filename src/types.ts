2075cb96f04ff8276a39202faa87bc4edb7b52a7
ITweeno <itweeno@gmail.com>
2023-03-17 15:23:25 +0000

    type fix

--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,32 @@
 export interface Address {
-    locality: string;
-    address_line_1: string;
-    country: string;
-    postal_code: string;
-    premises: string;
+  locality: string;
+  address_line_1: string;
+  country: string;
+  postal_code: string;
+  premises: string;
 }
 
 export interface Links {
-    self: string;
+  self: string;
 }
 
 export interface Matches {
-    snippet: any[];
-    title: number[];
+  snippet: string[];
+  title: number[];
 }
 
 export interface Company {
-    address: Address;
-    date_of_creation: string;
-    address_snippet: string;
-    company_status: string;
-    company_number: string;
-    kind: string;
-    company_type: string;
-    title: string;
-    description_identifier: string[];
-    links: Links;
-    matches: Matches;
-    description: string;
-    snippet: string;
+  address: Address;
+  date_of_creation: string;
+  address_snippet: string;
+  company_status: string;
+  company_number: string;
+  kind: string;
+  company_type: string;
+  title: string;
+  description_identifier: string[];
+  links: Links;
+  matches: Matches;
+  description: string;
+  snippet: string;
 }
