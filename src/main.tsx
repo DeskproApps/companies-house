9303bdd3d90cb2866f50db6b8444125b6357b1fd
ilia makarov <ilia.makarov@me.com>
2024-06-04 15:18:26 +0300

    Removed iframe-resizer

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
 
 ReactDOM.render(
   <React.StrictMode>
